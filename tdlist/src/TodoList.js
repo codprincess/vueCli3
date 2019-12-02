86b7cbeaff5b2bf627df2557d0f29257da0bfe63
jing <714195347@qq.com>
2019-12-02 11:52:35 +0800

    在方法里面添加action,用dispatch()传给store

--- tdlist/src/TodoList.js
+++ tdlist/src/TodoList.js
@@ -3,11 +3,11 @@ import 'antd/dist/antd.css'
 import { Input, Button ,List} from 'antd'
 import store from './store/index'
 
-const date = [
-    '早8点开晨会，分配今天的开发工作',
-    '早9点和项目经理作开发需求讨论会',
-    '晚5:30对今日代码进行review'
-]
+// const date = [
+//     '早8点开晨会，分配今天的开发工作',
+//     '早9点和项目经理作开发需求讨论会',
+//     '晚5:30对今日代码进行review'
+// ]
 class TodoList extends Component {
     constructor(props){
         super(props)
@@ -33,12 +33,20 @@ class TodoList extends Component {
     storeChange=()=>{
         this.setState(store.getState())
     }
+
+    //增加
+    clickBtn=()=>{
+        //在clickBtn方法里增加Action，然后用dispatch()方法传递给store
+        const action = {type:'addItem'}
+        store.dispatch(action)
+        console.log('1223456');
+    }
     render() {
         return (
             <div style={{ margin: '10px' }}>
                 <div>
                     <Input onChange={this.changeInputValue} placeholder={this.state.inputValue} style={{ width: '250px', marginRight: '10px'}} />
-                    <Button type="primary">增加</Button>
+                    <Button onClick={this.clickBtn} type="primary">增加</Button>
                 </div>
                 <div style={{margin:'10px',width:'300px'}}>
                     <List
--- tdlist/src/store/reducer.js
+++ tdlist/src/store/reducer.js
@@ -8,10 +8,28 @@ const defaultState = {
 }
 export default (state=defaultState,action)=>{
     console.log('store接收action传给reducer',state,action)
-    if(action.type === 'changInput'){
-        let newState = JSON.parse(JSON.stringify(state))
-        newState.inputValue = action.value
-        return newState
+    // if(action.type === 'changInput'){
+    //     let newState = JSON.parse(JSON.stringify(state))
+    //     newState.inputValue = action.value
+    //     return newState
+    // }
+    switch(action.type){
+        case 'changInput':
+           { 
+               let newState = JSON.parse(JSON.stringify(state))
+                newState.inputValue = action.value
+                return newState
+            }
+        case 'addItem':
+            {
+                let newState = JSON.parse(JSON.stringify(state))
+                newState.list.push(newState.inputValue)
+                newState.inputValue = ''
+                return newState
+            }
+            
+        default:
+            return state
     }
-    return state
+   
 }
