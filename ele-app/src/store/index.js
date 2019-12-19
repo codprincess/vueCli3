ca444202222052c225a73fabac32d5b37246227e
jing <714195347@qq.com>
2019-12-19 23:41:50 +0800

    获取地址

--- ele-app/src/store/index.js
+++ ele-app/src/store/index.js
@@ -3,13 +3,60 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const types ={
+  SET_LOCAIONT:"SET_LOCAIONT",
+  SET_ADDRESS:"SET_ADDRESS"
+}
+const state = {
+  location:{},
+  address:""
+}
+
+const getters = {
+  location:state=>state.location,
+  address:state=>state.address
+}
+
+const mutations = {
+  [types.SET_LOCAIONT](state,location){
+    if(location){
+      state.location = location
+    }else{
+      state.location = null
+    }
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  [types.SET_ADDRESS](state,address){
+    if(address){
+      state.address = address;
+    }else{
+      state.address = "";
+    }
   }
+}
+
+const actions = {
+  setLocation:({
+    commit
+  },location)=>{
+    commit(types.SET_LOCAIONT,location)
+  },
+  setAddress:({
+    commit
+  },address)=>{
+    commit(types.SET_ADDRESS,address)
+  }
+}
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
+  // modules: {
+  // }
 })
