8d16631fdcd9e6319de8f528e351ae8a71f137ae
jing <714195347@qq.com>
2020-03-06 13:40:13 +0800

    初始化

---
+++ douyin/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+//导入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
